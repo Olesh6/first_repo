0c80a328c6ea996ac8acee5df9e6bea2e80da47c
Olesh <olesh.ever@gmail.com>
2025-03-09 23:32:35 +0100

    First version of the application

---
+++ app.js
@@ -0,0 +1,12 @@
+import * as fs from 'node:fs';
+let a =JSON.stringify({text: 'this text', toketn:121, aa:"sdsdsdada"});
+
+console.log(typeof a);
+fs.writeFile('data.json', a, (err) => {
+    if (err) {
+      console.error('Error writing file:', err);  // Якщо сталася помилка
+    } else {
+      console.log('Data saved to data.json');  // Повідомлення про успіх
+    }
+  });	
+
